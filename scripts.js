73d7abbd302a9e2994a81ba8520fff4fb5f89c71
Jacob <jake_chambers12@hotmail.com>
2019-05-08 21:22:24 +0700

    touchups

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 $('#first').imagesLoaded({ background: true }, function () {
-    console.log('#container background image loaded');
-    // $("#first").fadeIn(3000);
-    // $("#introduction").fadeIn(1500);
+    $("#welcome").fadeOut(1500);
 
-    $("#welcome").fadeOut(1000);
-    // $("#welcome").addClass("hidden");
-    setTimeout(function () { $("#content").fadeIn(2000); }, 950);
+    setTimeout(function () { 
+        $("#content").fadeIn(1300);
+        $("body").removeClass("start");
+     }, 1430);
 
 
 });
