c0e77ef5c18ae99cc27a24cd9cadefd9bf81a250
Jacob <jake_chambers12@hotmail.com>
2019-05-10 17:39:15 +0700

    cleaner

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-$(document).ready(function () {
-    $("#welcome").fadeOut(1500);
-    setTimeout(function () {
-        $("#content").fadeIn(1300);
+// $('#first').imagesLoaded({ background: true }, function () {
+//     $("#welcome").fadeOut(1500);
+
+//     setTimeout(function () { 
+//         $("#content").fadeIn(1300);
+//         $("body").removeClass("start");
+//      }, 1430);
+// });
+
+$(window).on("load",function () {
+    console.log("ready!");
+    $("#welcome").fadeOut(1500, function () {
+        $("#content").fadeIn(1500);
         $("body").removeClass("start");
-    }, 1430);
+    });
 });
