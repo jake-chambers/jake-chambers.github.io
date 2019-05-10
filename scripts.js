6194520a2679c715680104abe681974d372110d4
Jacob <jake_chambers12@hotmail.com>
2019-05-10 17:54:31 +0700

    this is the best way

--- scripts.js
+++ scripts.js
@@ -1,16 +1,7 @@
-// $('#first').imagesLoaded({ background: true }, function () {
-//     $("#welcome").fadeOut(1500);
-
-//     setTimeout(function () { 
-//         $("#content").fadeIn(1300);
-//         $("body").removeClass("start");
-//      }, 1430);
-// });
-
-$(window).on("load",function () {
-    console.log("ready!");
-    $("#welcome").fadeOut(1500, function () {
-        $("#content").fadeIn(1500);
+$('#first').imagesLoaded({ background: true }, function () {
+    $("#welcome").fadeOut(1500);
+    setTimeout(function () { 
+        $("#content").fadeIn(1300);
         $("body").removeClass("start");
-    });
+     }, 1430);
 });
