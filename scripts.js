40830747fb7b0bc40efc23420852d48566914c1f
Jacob <jake_chambers12@hotmail.com>
2019-05-11 12:50:43 +0700

    scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-$('#first').imagesLoaded({ background: true }, function () {
-    $("#welcome").fadeOut(1500);
-    setTimeout(function () { 
-        $("#content").fadeIn(1300);
-        $("body").removeClass("start");
-     }, 1430);
-});
+$(window).on("load",function () {
+    $('#first').imagesLoaded({ background: true }, function () {
+        $("#welcome").fadeOut(1500);
+        setTimeout(function () {
+            $("#content").fadeIn(1300);
+            $("body").removeClass("start");
+        }, 1430);
+    });
+});  
