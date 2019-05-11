31ee0cc746739a8467efd673b17eb47a82e42d5d
Jacob <jake_chambers12@hotmail.com>
2019-05-12 00:12:01 +0700

    safari is annoying

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 $(window).on("load",function () {
     $('#first').imagesLoaded({ background: true }, function () {
-        $("#welcome").fadeOut(1500);
+        $("#welcome").fadeOut(1900);
         setTimeout(function () {
             $("#content").fadeIn(1300);
-            $("body").removeClass("start");
-        }, 1430);
+        }, 1830);
     });
 });  
+
