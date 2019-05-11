4ac2cf7ac920487673911115b625ad7b7f136365
Jacob <jake_chambers12@hotmail.com>
2019-05-12 00:22:00 +0700

    test

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 $(window).on("load",function () {
-    $('#first').imagesLoaded({ background: true }, function () {
+    // $('#first').imagesLoaded({ background: true }, function () {
         $("#welcome").fadeOut(1900);
         setTimeout(function () {
             $("#content").fadeIn(1300);
         }, 1830);
-    });
+    // });
 });  
 
+// window.addEventListener("load",function(){
+//     $('#first').imagesLoaded({ background: true }, function () {
+//         $("#welcome").fadeOut(1900);
+//         setTimeout(function () {
+//             $("#content").fadeIn(1300);
+//         }, 1830);
+//     });
+// })
